d75991052f1902e33d4a11d35168a0c7f0054ccd
debauchery1st <trevor.j.martin@gmail.com>
2019-12-17 00:57:13 -0500

    apple style top navbar

---
+++ apple-nav/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import {WrapperDiv, NAV, Button, SubNav, CropDiv32, CropDiv64} from './components'
+
+const randomKey = () => String(Math.random()).split(".")[1];
+
+const navItem = (item) => {  
+  return(
+  <Link key={randomKey()} to={item.url} onClick={item.onClick} ><CropDiv64><Button key={randomKey()}>{item.text}</Button></CropDiv64></Link>
+  )
+}
+
+
+const exampleMenu = () => {
+  const items = [
+    {url: "/", text: <img src="/apple.svg" />},
+    {url: "/Mac", text: "Mac"},
+    {url: "/iPad", text: "iPad"},
+    {url: "/iPhone", text: "iPhone"},
+    {url: "/Watch", text: "Watch"},
+    {url: "/TV", text: "TV"},
+    {url: "/Music", text: "Music"},
+    {url: "/Support", text: "Support"},
+    {url: "/Search", text: (<CropDiv32> <img src="/search.svg" height="64px" /> </CropDiv32>)},
+    {url: "/Store", text: <img src="/shop.svg" height="32px" />},
+  ];
+  return (items.map(navItem))
+}
+
+function App() {
+  const [subState, setSubState] = useState("");
+  const [subContent, setSubContent] = useState([]);
+  function changeState(txt){
+    console.log("change state")
+    setSubState(txt);
+  }
+  useEffect(() => {
+    console.log("App.useEffect")
+    setSubContent("sub content: " + subState);
+  }, [subState])
+
+
+  const navMenu = exampleMenu(changeState);
+  return (
+    <WrapperDiv>
+      <div className="App">
+        <header className="App-header">
+          <NAV>
+          {navMenu}
+          </NAV>
+      </header>
+      <SubNav>{subContent}</SubNav>
+      </div>
+    </WrapperDiv>
+  )
+}
+
+export default App;
